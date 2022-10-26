f460bd273a5693be38f9a3c8c830c6fd1d3faa85
Dennis Yang <dennisyang3.14@gmail.com>
2022-10-26 11:25:41 +0100

    Checks user input is a single character and letter

--- milestone_2.py
+++ milestone_2.py
@@ -1,9 +1,13 @@
 # %%
 import random
 word_list = ["banana", "apple", "orange", "strawberry", "pineapple"]
-print(word_list)
 word = random.choice(word_list)
-word
 guess = input("Please enter a single character")
 
-# %%
+if guess.isdigit():
+    print("Oops that is not a valid input.")
+elif len(guess) ==  1:
+    print("Good guess!")
+else:
+    print("Oops that is not a valid input. ")
+ # %%
