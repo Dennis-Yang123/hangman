6939a0a721589bb63eecc8d1227dbbb5157577f6
Dennis Yang <dennisyang3.14@gmail.com>
2022-10-26 11:33:25 +0100

    Checks if it's a letter instead because taskbot

--- milestone_2.py
+++ milestone_2.py
@@ -4,7 +4,7 @@ word_list = ["banana", "apple", "orange", "strawberry", "pineapple"]
 word = random.choice(word_list)
 guess = input("Please enter a single character")
 
-if guess.isdigit():
+if guess.isalpha() == False:
     print("Oops that is not a valid input.")
 elif len(guess) ==  1:
     print("Good guess!")
