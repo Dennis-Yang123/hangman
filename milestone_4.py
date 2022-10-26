d85ee648ac9e99018d5eb6231a4ff0e7d0ff8ccb
Dennis Yang <dennisyang3.14@gmail.com>
2022-10-26 17:36:48 +0100

    Made class and initialised attributes

--- milestone_4.py
+++ milestone_4.py
@@ -1,1 +1,13 @@
-print("Hello world")
+# %%
+import random
+num_lives = 5
+class Hangman:
+    def __init__(self, word_list, num_lives, word, word_guessed, num_letters, list_of_guesses):
+        self.word_list = word_list
+        self.num_lives = num_lives
+        self.word = words
+        self.word_guessed = word_guessed
+        self.num_letters = num_letters
+        self.list_of_gusses = list_of_guesses
+
+# %%
