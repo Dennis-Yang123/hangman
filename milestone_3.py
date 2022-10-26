a975810f3976a0dc011655ce0e78058510230a8c
Dennis-Yang123 <116642117+Dennis-Yang123@users.noreply.github.com>
2022-10-26 12:39:59 +0100

    Add files via upload

---
+++ milestone_3.py
@@ -0,0 +1,14 @@
+# %%
+
+
+
+while True:
+    guess = input("Please enter a guess that is a single character and alphabetical letter")
+    if guess.isalpha() == False:
+        print("Invalid letter. Please, enter a single alphabetical character.")
+    elif len(guess) == 1:
+        break
+    else:
+        print("Invalid letter. Please, enter a single alphabetical character.")
+
+# %%
