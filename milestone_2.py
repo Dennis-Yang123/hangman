7a2b8b907232f771fdc34b05f6c950372b8317dc
Dennis Yang <dennisyang3.14@gmail.com>
2022-10-26 11:09:59 +0100

    Asks user for a single character input

--- milestone_2.py
+++ milestone_2.py
@@ -4,4 +4,6 @@ word_list = ["banana", "apple", "orange", "strawberry", "pineapple"]
 print(word_list)
 word = random.choice(word_list)
 word
+guess = input("Please enter a single character")
+
 # %%
