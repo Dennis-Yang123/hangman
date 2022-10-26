18af307b059e318696a13a8ce5a2e6947313fadf
Dennis Yang <dennisyang3.14@gmail.com>
2022-10-26 15:32:36 +0100

    Uses functions to check

--- milestone_3.py
+++ milestone_3.py
@@ -3,20 +3,27 @@ import random
 
 word_list = ["banana", "apple", "orange", "strawberry", "pineapple"]
 word = random.choice(word_list)
-while True:
-    guess = input("Please enter a guess that is a single character and alphabetical letter")
-    if guess.isalpha() == False:
-        print("Invalid letter. Please, enter a single alphabetical character.")
-    elif len(guess) == 1:
-        if guess in word:
+
+def check_guess(guess):
+    guess.lower()
+    if guess in word:
             print("Good guess!", guess, "is in the word.")
-            break
-            
-        else:
-            print("Sorry", guess, "is not in the word.")
             
     else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+        print("Sorry", guess, "is not in the word.")
 
+
+def ask_for_input():
+    while True:
+        guess = input("Please enter a guess that is a single character and alphabetical letter")
+        if guess.isalpha() == False:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+        elif len(guess) == 1:
+            check_guess(guess)
+                
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")    
+
+ask_for_input()
 
 # %%
