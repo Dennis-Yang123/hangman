695c59edecbae08a534d1fddf1d90cbbf37da630
Dennis Yang <dennisyang3.14@gmail.com>
2022-10-26 12:44:48 +0100

    while loop

--- milestone_3.py
+++ milestone_3.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 # %%
-
-
-
 while True:
     guess = input("Please enter a guess that is a single character and alphabetical letter")
     if guess.isalpha() == False:
@@ -13,19 +9,3 @@ while True:
         print("Invalid letter. Please, enter a single alphabetical character.")
 
 # %%
-=======
-# %%
-
-
-
-while True:
-    guess = input("Please enter a guess that is a single character and alphabetical letter")
-    if guess.isalpha() == False:
-        print("Invalid letter. Please, enter a single alphabetical character.")
-    elif len(guess) == 1:
-        break
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
-
-# %%
->>>>>>> a975810f3976a0dc011655ce0e78058510230a8c
