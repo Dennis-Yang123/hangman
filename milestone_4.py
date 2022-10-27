8cb7dc1b14de839e885c3cef4f5976c9a7ee6550
Dennis Yang <dennisyang3.14@gmail.com>
2022-10-27 17:27:52 +0100

    feedback check

--- milestone_4.py
+++ milestone_4.py
@@ -10,23 +10,21 @@ word = random.choice(word_list)
 class Hangman:
     def __init__(self, word_list, word, word_guessed, num_letters, list_of_guesses, num_lives = 5):
         self.word_list = word_list
-        self.num_lives = num_lives
         self.word = word 
         self.word_guessed = word_guessed 
         self.num_letters = num_letters 
-        self.list_of_guesses = list_of_guesses 
+        self.list_of_guesses = list_of_guesses
+        self.num_lives = num_lives
 
     def check_guess(self, guess):
-        self.guess.lower()
-        if self.guess in self.word:
-            print("Good guess!", self.guess, "is in the word.")
+        guess.lower()
+        if guess in self.word:
+            print("Good guess!", guess, "is in the word.")
                 
         else:
             print("Sorry", guess, "is not in the word.")
 
     def ask_for_input(self):
-        list_of_guesses = []
-        
         while True:
             guess = input("Please enter a guess that is a single character and alphabetical letter")
             if guess.isalpha() == False:
@@ -35,18 +33,17 @@ class Hangman:
             elif len(guess) != 1:
                 print("Invalid letter. Please, enter a single alphabetical character.")
 
-            elif guess in list_of_guesses:
+            elif guess in self.list_of_guesses:
                 print("You've already tried that letter!")
             
                 
             else:
                 self.check_guess(guess)
-                list_of_guesses.extend(guess)
+                self.list_of_guesses.extend(guess)
                 break
     
-    
+abc = Hangman    
 
 
-abc = Hangman
 
 # %%
