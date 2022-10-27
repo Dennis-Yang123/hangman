4fcb19d139a3d0aff260b0383533f2b37516c6f3
Dennis Yang <dennisyang3.14@gmail.com>
2022-10-27 11:25:35 +0100

    feedback check

--- milestone_4.py
+++ milestone_4.py
@@ -11,7 +11,10 @@ class Hangman():
     def __init__(self, word_list, num_lives = 5):
         self.word_list = word_list
         self.num_lives = num_lives
-        
+        self.word = word
+        self.word_guessed = word_guessed
+        self.num_letters = num_letters
+        self.list_of_guesses = list_of_guesses
     def check_guess(self, guess):
         self.guess.lower()
         if self.guess in self.word:
