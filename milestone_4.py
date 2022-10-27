a96eb3683d141fe14aa363455c7ec3d46bcc01ae
Dennis Yang <dennisyang3.14@gmail.com>
2022-10-27 18:56:32 +0100

    class initialised

--- milestone_4.py
+++ milestone_4.py
@@ -11,10 +11,10 @@ class Hangman:
     def __init__(self, word_list, num_lives = 5):
         self.word_list = word_list
         self.word = random.choice(word_list)
-        self.word_guessed = ["_"] * len(word)
-        self.num_letters = len(set(word)) 
+        self.word_guessed = ["_"] * len(self.word)
+        self.num_letters = len(set(self.word)) 
         self.list_of_guesses = []
-        self.num_lives = num_lives
+        
 
     def check_guess(self, guess):
         guess.lower()
