c379a66108c9e713bbeff1671041f03a0a40e225
Dennis Yang <dennisyang3.14@gmail.com>
2022-10-27 11:46:06 +0100

    feedback check

--- milestone_4.py
+++ milestone_4.py
@@ -11,10 +11,10 @@ class Hangman():
     def __init__(self, word_list, word, word_guessed, num_letters, list_of_guesses, num_lives = 5):
         self.word_list = word_list
         self.num_lives = num_lives
-        self.word = word
-        self.word_guessed = word_guessed
-        self.num_letters = num_letters
-        self.list_of_guesses = list_of_guesses
+        self.word = word = str
+        self.word_guessed = word_guessed = str
+        self.num_letters = num_letters = int
+        self.list_of_guesses = list_of_guesses = list
 
 
 # %%
